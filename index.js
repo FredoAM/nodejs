a2eeaba9838de62aee0fa72439a8a71b2be79730
Alfredo Aranda <fredoaranda@gmail.com>
2023-12-09 12:49:39 -0600

    2nd commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const http = require('http');
+// const http = require('http');
+// const PORT = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}/`);
+// });
+
+const express = require('express');
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
