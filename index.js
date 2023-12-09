7e2b73c3e53dfcdc15ae6e372061789ce2d68532
Alfredo Aranda <fredoaranda@gmail.com>
2023-12-09 12:59:17 -0600

    4th try

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const http = require('http');
-// const PORT = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World!');
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
-
-// const express = require('express');
-// const PORT = 3000;
-
-// const app = express();
-
-// app.get('/', (req, res) => {
-//   res.status(200).send('Hello World!');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
-
 const express = require('express');
 const path = require('path');
 const PORT = 3000;
